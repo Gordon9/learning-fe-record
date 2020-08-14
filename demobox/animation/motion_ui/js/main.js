c4cb8110c9b3e5d68848e8364ee095c0fa50bc23
Gordon <gordnaman@gmail.com>
2020-08-14 20:33:27 +0800

    Motion UI Demo done

--- demobox/animation/motion_ui/js/main.js
+++ demobox/animation/motion_ui/js/main.js
@@ -68,9 +68,66 @@ document.querySelectorAll('.nav-link').forEach(link => {
 });
 
 anime.timeline().add({
-    targets: ".main-header",
-    opacity: [0, 1],
-    translateY: ["-5rem", 0],
-    easing: "spring(1, 80, 10, 0)",
-    duration: 300
-});
+        targets: ".main-header",
+        opacity: [0, 1],
+        translateY: ["-5rem", 0],
+        easing: "spring(1, 80, 10, 0)",
+        duration: 300
+    })
+    .add({
+        targets: ['.nav-link', '.brand'],
+        opacity: [0, 1],
+        translateY: ["-2rem", 0],
+        easing: "spring(1, 80, 10, 0)",
+        duration: 500
+    }, "-=800")
+    .add({
+        targets: ['.title-header .line'],
+        easing: "easeInOutExpo",
+        duration: 600,
+        translateY: (el, i) => -0.625 + 0.625 * 2 * i + 'em'
+    }, "-=700")
+    .add({
+        targets: ['.title-header .letters-left'],
+        opacity: [0, 1],
+        translateX: ["0.65em", "0.15em"],
+        easing: "easeOutExpo",
+        duration: 600
+    }, "-=300")
+    .add({
+        targets: ['.title-header .letters-right'],
+        opacity: [0, 1],
+        translateX: ["-0.65em", "-0.15em"],
+        easing: "easeOutExpo",
+        duration: 600
+    }, "-=600");
+
+let observe;
+const article = Array.from(document.querySelectorAll('.article'));
+let options = {
+    threshold: 0.5
+}
+let current = -1;
+
+observer = new IntersectionObserver(entries => {
+    entries.forEach(entry => {
+        if (entry.intersectinoRatio > 0.5) {
+            const newcurrent = aricles.indexOf(entry.target);
+            if (newcurrent > current) {
+                anime({
+                    targets: entry.target,
+                    opacity: [0, 1],
+                    translateY: ["20rem", 0],
+                    easing: "spring(1, 80, 10, 0)",
+                    duration: 800
+                });
+            }
+            current = newcurrent;
+            observer.unobserve(entry.target);
+        }
+    });
+}, options);
+
+document.querySelectorAll(".article").forEach(article => {
+    observer.observe(aricle);
+})
