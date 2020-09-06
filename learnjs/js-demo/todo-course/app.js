c942b9d11643032832bd7702acf80830eeb35998
Gordon <gordnaman@gmail.com>
2020-09-06 20:39:42 +0800

    crying infront of my computer & basic js completed

---
+++ learnjs/js-demo/todo-course/app.js
@@ -0,0 +1,34 @@
+// Selectors
+const todoInput = document.querySelector(".todo-input");
+const todoButton = document.querySelector(".todo-button");
+const todoList = document.querySelector(".todo-list");
+
+// Event Listeners
+todoButton.addEventListener("click", addTodo);
+
+// Functions
+function addTodo(event) {
+  // Prevent form from submtting
+  event.preventDefault();
+  //   Todo Div
+  const todoDiv = document.createElement("div");
+  todoDiv.classList.add("todo");
+  //   Create LI
+  const newTodo = document.createElement("li");
+  newTodo.innerText = "Hello";
+  newTodo.classList.add("todo-item");
+  todoDiv.appendChild(newTodo);
+  //   CHECK MARK BUTTON
+  const completedButton = document.createElement("button");
+  completedButton.innerHTML = '<i class="fas fa-check"></i>';
+  completedButton.classList.add("completed-btn");
+  todoDiv.appendChild(completedButton);
+  //   CHECK TRASH BUTTON
+  const trashButton = document.createElement("button");
+  trashButton.innerHTML = '<i class="fas fa-trash"></i>';
+  trashButton.classList.add("trash-btn");
+  todoDiv.appendChild(trashButton);
+
+  //   APPEND TO LIST
+  todoList.appendChild(todoDiv);
+}
