7e904f6040190eeec9f412a26b563306fac67711
Gordon <gordnaman@gmail.com>
2020-09-22 19:58:02 +0800

    25 event done

---
+++ js30/25 - Event/app.js
@@ -0,0 +1,12 @@
+const divs = document.querySelectorAll("div");
+
+function logText(e) {
+  console.log(this.classList.value);
+}
+
+divs.forEach((div) =>
+  div.addEventListener("click", logText, {
+    capture: false,
+    once: false,
+  })
+);
