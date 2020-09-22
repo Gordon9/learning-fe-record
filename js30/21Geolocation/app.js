511b36f09ffa2dfa96918205a95e7571d65cb047
Gordon <gordnaman@gmail.com>
2020-09-22 13:52:21 +0800

    21 geolocation done

---
+++ js30/21Geolocation/app.js
@@ -0,0 +1,13 @@
+const arrow = document.querySelector(".arrow");
+const speed = document.querySelector(".speed-value");
+
+navigator.geolocation.watchPosition(
+  (data) => {
+    console.log("data:", data);
+    speed.textContent = data.coords.speed;
+    arrow.style.transform = `rotate(${data.coords.heading}deg)`;
+  },
+  (err) => {
+    console.log(err);
+  }
+);
