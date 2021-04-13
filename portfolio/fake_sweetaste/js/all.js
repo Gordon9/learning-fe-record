9fcdc83243aff508eaf23afcc1a2613ca73c2cba
Gordon <gordnaman@gmail.com>
2021-04-14 01:56:15 +0800

    slide mobile_menu

--- portfolio/fake_sweetaste/js/all.js
+++ portfolio/fake_sweetaste/js/all.js
@@ -1,8 +1,15 @@
 const hamburger = document.querySelector(".mobile_menu .hamburger");
 // const mobileUl = document.querySelector(".mobile_menu ul");
 const mobileMenu = document.querySelector(".mobile_menu");
+const eleMore = document.querySelector(".container_ul");
+
+let display = false;
 
 hamburger.addEventListener("click", () => {
   console.log("hello");
   mobileMenu.classList.toggle("active");
+
+  display = !display;
+  eleMore.style.height = display ? "217px" : "0px";
+  return false;
 });
