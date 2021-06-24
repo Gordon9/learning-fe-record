9a80490b10a488b822bd73fd69402e7cef97919f
Gordontam <gordnaman@gmail.com>
2021-06-24 19:57:34 +0800

    delete action done

--- demo/react-todo/src/App.js
+++ demo/react-todo/src/App.js
@@ -1,10 +1,23 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import Form from "./components/Form";
+import TodoList from "./components/TodoList";
 
 function App() {
+  const [inputText, setInputText] = useState("");
+  const [todos, setTodos] = useState([]);
   return (
     <div className="App">
-      <h1>React ToDo</h1>
+      <header>
+        <h1>React ToDo</h1>
+      </header>
+      <Form
+        inputText={inputText}
+        todos={todos}
+        setTodos={setTodos}
+        setInputText={setInputText}
+      />
+      <TodoList todos={todos} setTodos={setTodos}  />
     </div>
   );
 }
