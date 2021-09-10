32d5e96a27e8556c10902e25ce210c92218d6c97
Gordon <gordnaman@gmail.com>
2021-09-10 23:15:16 +0800

    handle menu&navbar

--- toy/hospital/js/app.js
+++ toy/hospital/js/app.js
@@ -1,2 +1,12 @@
 const menu = document.querySelector("#menu-btn");
 const navbar = document.querySelector(".navbar");
+
+menu.addEventListener("click", () => {
+  menu.classList.toggle("fa-times");
+  navbar.classList.toggle("active");
+});
+
+window.onscroll = () => {
+  menu.classList.remove("fa-times");
+  navbar.classList.remove("active");
+};
