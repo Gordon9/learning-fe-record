b8e1523acb4b706d2c6f08d61d72f5d768e6f016
Gordon <gordnaman@gmail.com>
2021-10-20 22:42:53 +0800

    add json-server

--- study_react/demo/demo3/src/components/Task.js
+++ study_react/demo/demo3/src/components/Task.js
@@ -1,18 +1,18 @@
 import { FaTimes } from "react-icons/fa";
 
 const Task = ({ task, onDelete, onToggle }) => {
-  return (
-    <div
-      className={`task ${task.reminder ? "reminder" : ""}`}
-      onDoubleClick={() => onToggle(task.id)}
-    >
-      <h3>
-        {task.text}
-        <FaTimes onClick={() => onDelete(task.id)} />
-      </h3>
-      <p>{task.day}</p>
-    </div>
-  );
+  return <>hello</>;
 };
 
 export default Task;
+
+// <div
+// className={`task ${task.reminder ? "reminder" : ""}`}
+// onDoubleClick={() => onToggle(task.id)}
+// >
+// <h3>
+//   {task.text}
+//   <FaTimes onClick={() => onDelete(task.id)} />
+// </h3>
+// <p>{task.day}</p>
+// </div>
