176354a095325d610250db4c50f74cc3b320d5f6
Gordon <gordnaman@gmail.com>
2021-10-21 20:58:54 +0800

    add async-await

--- toy/random_joke_gen/app.js
+++ toy/random_joke_gen/app.js
@@ -2,15 +2,26 @@ const joke = document.querySelector(".joke");
 const btn = document.querySelector(".btn");
 const url = "http://api.icndb.com/jokes/random";
 
-const getJoke = () => {
-  fetch(url)
-    .then((response) => {
-      return response.json();
-    })
-    .then((data) => {
-      console.log(data);
-      joke.innerHTML = data.value.joke;
-    });
+// const getJoke = () => {
+//   fetch(url)
+//     .then((response) => {
+//       return response.json();
+//     })
+//     .then((data) => {
+//       console.log(data);
+//       joke.innerHTML = data.value.joke;
+//     });
+// };
+
+// async await
+const getJoke = async () => {
+  try {
+    const response = await fetch(url);
+    const data = await response.json();
+    joke.innerHTML = data.value.joke;
+  } catch (error) {
+    joke.innerHTML = error.message;
+  }
 };
 
 btn.addEventListener("click", getJoke);
