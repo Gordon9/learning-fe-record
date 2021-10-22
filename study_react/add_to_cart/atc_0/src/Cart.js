5934ad4bbc7d96d7d8bff8de4387680343ef203c
Gordon <gordnaman@gmail.com>
2021-10-22 21:33:07 +0800

    add showitems layout

--- study_react/add_to_cart/atc_0/src/Cart.js
+++ study_react/add_to_cart/atc_0/src/Cart.js
@@ -18,6 +18,34 @@ const Cart = () => {
     <section>
       <p>Cart unique items ({totalUniqueItems})</p>
       <p>Total Item: {totalItems}</p>
+      <ul>
+        {items.map((item, index) => {
+          return (
+            <li class="flex bg-white p-5 rounded-lg shadow-lg mb-5">
+              <img src={item.img} class="w-32" alt="" />
+              <div class="flex-grow flex flex-col md:flex-row items-center justify-center md:justify-between">
+                <p class="title font-semibold text-sm md:text-lg mb-5 md:mb-0 md:pl-5">
+                  {item.title}
+                </p>
+
+                {/* <div class="flex">
+                  <button class="fa fa-minus rounded-lg bg flex justify-center items-center p-3 z-10"></button>
+                  <input
+                    type="number"
+                    value="1"
+                    class="text-center text-md font-semibold p-2 rounded w-20 focus:outline-none"
+                  />
+                  <button class="fa fa-plus rounded-lg bg flex justify-center items-center p-3 z-10"></button>
+                </div> */}
+                <p class="value font-bold text-sm mt-5 md:mt-0">
+                  quantity ({item.quantity})
+                </p>
+                <p class="value font-bold text-sm mt-5 md:mt-0">{item.price}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
     </section>
   );
 };
