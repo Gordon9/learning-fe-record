16ab4d245512bcae32a2669cd61180617bdfc291
Gordon <gordnaman@gmail.com>
2021-10-22 19:32:44 +0800

    fixed the picture not showing

--- study_react/add_to_cart/atc_0/src/Home.js
+++ study_react/add_to_cart/atc_0/src/Home.js
@@ -4,21 +4,21 @@ import Itemcard from "./Itemcard";
 import data from "./data";
 
 const Home = () => {
-  // console.log(data.productData);
-
   return (
     <>
       <h1 className="text-center mt-3 text-3xl">All Items</h1>
       <section className="lg:flex mb-4 m-10 rounded mx-auto">
         {data.productData.map((product) => {
-          // const { img, id } = product;
-          console.log(product.img);
-
-          return <Itemcard img={product.img} key={product.id} />;
+          return (
+            <Itemcard
+              img={product.img}
+              key={product.id}
+              title={product.title}
+              price={product.price}
+            />
+          );
         })}
       </section>
-      <img src="./pic/orange.jpeg" alt="" className="h-full" />
-      <div>hello</div>
     </>
   );
 };
--- study_react/add_to_cart/atc_0/src/Itemcard.js
+++ study_react/add_to_cart/atc_0/src/Itemcard.js
@@ -1,18 +1,14 @@
 import React from "react";
 import "./App_tw.css";
 
-const Itemcard = (img) => {
-  console.log(img);
-
+const Itemcard = ({ img, title, price }) => {
   return (
     <>
       <div className="w-full lg:w-1/3 rounded overflow-hidden shadow-lg m-10 relative flex flex-col">
         <img className="w-full" src={img} alt="Sunset in the mountains" />
         <div className="px-6 py-4 h-full">
-          <div className="font-bold text-xl mb-2">The Coldest Sunset</div>
-          <p className="text-gray-700 text-base">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit.
-          </p>
+          <div className="font-bold text-xl mb-2">{title}</div>
+          <p className="text-gray-700 text-base">{price}</p>
         </div>
         <button className="w-full bg-gray-300 hover:bg-gray-400 text-gray-800 font-bold py-2 px-4 rounded inline-flex items-center">
           <span>Add to Cart</span>
