bb0df688d66fbee61853e7e664e6e0eb2801d5a9
Gordon <gordnaman@gmail.com>
2021-10-22 21:52:05 +0800

    add remove-btn handle

--- study_react/add_to_cart/atc_0/src/Cart.js
+++ study_react/add_to_cart/atc_0/src/Cart.js
@@ -15,7 +15,7 @@ const Cart = () => {
 
   if (isEmpty) return <h1 className="text-center">Your cart is empty</h1>;
   return (
-    <section>
+    <section className="container py-4 px-10">
       <p>Cart unique items ({totalUniqueItems})</p>
       <p>Total Item: {totalItems}</p>
       <ul>
@@ -27,20 +27,45 @@ const Cart = () => {
                 <p class="title font-semibold text-sm md:text-lg mb-5 md:mb-0 md:pl-5">
                   {item.title}
                 </p>
-
-                {/* <div class="flex">
-                  <button class="fa fa-minus rounded-lg bg flex justify-center items-center p-3 z-10"></button>
-                  <input
-                    type="number"
-                    value="1"
-                    class="text-center text-md font-semibold p-2 rounded w-20 focus:outline-none"
-                  />
-                  <button class="fa fa-plus rounded-lg bg flex justify-center items-center p-3 z-10"></button>
-                </div> */}
                 <p class="value font-bold text-sm mt-5 md:mt-0">
                   quantity ({item.quantity})
                 </p>
                 <p class="value font-bold text-sm mt-5 md:mt-0">{item.price}</p>
+                <div class="flex">
+                  <button
+                    class="rounded-lg bg flex justify-center items-center p-3 z-10"
+                    style={{ backgroundColor: "#00848A" }}
+                    onClick={() => {
+                      updateItemQuantity(item.id, item.quantity - 1);
+                    }}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    value="1"
+                    class="text-center text-md font-semibold p-2 rounded w-20 focus:outline-none"
+                    style={{ backgroundColor: "#eee" }}
+                  />
+                  <button
+                    class="rounded-lg bg flex justify-center items-center p-3 z-10"
+                    style={{ backgroundColor: "#00848A" }}
+                    onClick={() => {
+                      updateItemQuantity(item.id, item.quantity + 1);
+                    }}
+                  >
+                    +
+                  </button>
+                  <button
+                    class="rounded-lg bg flex justify-center items-center p-3 z-10 ml-14"
+                    style={{ backgroundColor: "#FF5A5F" }}
+                    onClick={() => {
+                      removeItem(item.id);
+                    }}
+                  >
+                    Remove Item
+                  </button>
+                </div>
               </div>
             </li>
           );
