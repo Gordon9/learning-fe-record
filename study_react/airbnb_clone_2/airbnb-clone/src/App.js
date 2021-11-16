1516db19405f60bf283b00ae89d030c32fe5357f
Gordon <gordnaman@gmail.com>
2021-11-16 23:02:48 +0800

    init airbnb-clone

---
+++ study_react/airbnb_clone_2/airbnb-clone/src/App.js
@@ -0,0 +1,12 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Let's build Airbnb-clone</h1>
+      <h2>good night, gordon</h2>
+    </div>
+  );
+}
+
+export default App;
