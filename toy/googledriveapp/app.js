aa40203ab7748208049964e8749a18e23d8f5006
Gordon <gordnaman@gmail.com>
2021-11-28 17:23:11 +0800

    add audio&video handle

--- toy/googledriveapp/app.js
+++ toy/googledriveapp/app.js
@@ -12,7 +12,6 @@ const generateLink = (e) => {
   const confirmLink = gLinkValue.includes("https://drive.google.com/file/d/");
 
   if (confirmLink) {
-    console.log("good");
     const getDownloadLink = gLinkValue
       .replace(
         "https://drive.google.com/file/d/",
@@ -35,6 +34,30 @@ const generateLink = (e) => {
     const copy = document.querySelector(".copy");
     copy.addEventListener("click", () => {
       return copyText(downloadLink);
+    });
+
+    // EMBED AUDIO
+    const AUDIO1 = '<audio width="300" height="32" controls="controls" src="';
+    const AUDIO2 = '" type="audio/mp3"</audio>';
+
+    embedAudio.value = `${AUDIO1}${downloadLink.value}${AUDIO2}`;
+    // COPY EMBED AUDIO CODE
+    const copyAudio = document.querySelector(".copy-audio");
+    copyAudio.addEventListener("click", () => {
+      return copyText(embedAudio);
+    });
+
+    // EMBED VIDEO
+    const getVideoLink = glink.value.replace("/view?usp=sharing", "");
+
+    const VIDEO1 = '<iframe src="';
+    const VIDEO2 = '/preview" width="560" height="315"><iframe/>';
+
+    embedVideo.value = `${VIDEO1}${getVideoLink}${VIDEO2}`;
+    // COPY EMBED VIDEO CODE
+    const copyVideo = document.querySelector(".copy-video");
+    copyVideo.addEventListener("click", () => {
+      return copyText(embedVideo);
     });
   } else {
     alert("Please Enter a Google Drive File Link");
