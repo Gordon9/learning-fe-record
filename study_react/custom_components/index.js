9234925087bbfe3d8a9d8342e30ca4f6492265de
Gordon <gordnaman@gmail.com>
2022-03-02 09:59:15 +0800

    custom_components

---
+++ study_react/custom_components/index.js
@@ -0,0 +1,19 @@
+function Page() {
+  return (
+    <div>
+      <header>
+        <img src="./3500.jpg" width="200px" alt="" />
+      </header>
+      <h1>Brothers mind</h1>
+      <ul>
+        <li>221b</li>
+        <li>The Game is On</li>
+        <li>No flowers.</li>
+        <li>My request</li>
+      </ul>
+      <footer>&copy; 2022 BugBugGoaway</footer>
+    </div>
+  );
+}
+
+ReactDOM.render(<Page />, document.getElementById("root"));
