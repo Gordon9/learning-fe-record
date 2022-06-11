a418922376ec3877a01f8eb08071c951118b95f4
Gordon <gordnaman@gmail.com>
2022-06-11 21:41:29 +0800

    e_shop done

--- fake/ecommerce_website/e_shop/app.js
+++ fake/ecommerce_website/e_shop/app.js
@@ -10,3 +10,36 @@ const swiper = new Swiper(".mySwiper", {
     clickable: true,
   },
 });
+
+const elem = document.querySelector(".product-wrapper");
+const iso = new Isotope(elem, {
+  itemSelector: ".product-card",
+  layoutMode: "fitRows",
+});
+
+const filtersElem = document.querySelector("#filter");
+filtersElem.addEventListener("click", function (event) {
+  if (!matchesSelector(event.target, "button")) {
+    return;
+  }
+  const filterValue = event.target.getAttribute("data-filter");
+  iso.arrange({ filter: filterValue });
+});
+
+// change is-checked class on buttons
+let buttonGroups = document.querySelector(".filter-group");
+let len = buttonGroups.clientHeight;
+for (let i = 0; i < len; i++) {
+  let buttonGroup = buttonGroups[i];
+  radioButtonGroup(buttonGroup);
+}
+
+function radioButtonGroup(buttonGroup) {
+  buttonGroups.addEventListener("click", function (event) {
+    if (!matchesSelector(event.target, "button")) {
+      return;
+    }
+    buttonGroups.querySelector(".is-checked").classList.remove("is-checked");
+    event.target.classList.add("is-checked");
+  });
+}
