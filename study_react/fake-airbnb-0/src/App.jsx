b8c246c0194ca5126dd4aa7a8c77e387c5eb564e
Gordon <gordnaman@gmail.com>
2022-12-01 21:59:24 +0800

    hero added

--- study_react/fake-airbnb-0/src/App.jsx
+++ study_react/fake-airbnb-0/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Navbar from './components/Navbar.jsx'
+import Hero from './components/Hero.jsx'
 
 function App() {
 
   return (
     <div className="App">
       <Navbar /> 
+      <Hero />
     </div>
   )
 }
---
+++ study_react/fake-airbnb-0/src/components/Hero.jsx
@@ -0,0 +1,11 @@
+import photo_grid from '../assets/images/photo-grid.png';
+
+export default function Hero() {
+  return (
+    <section className='hero'>
+      <img src={photo_grid} alt="photo-grid" className='hero--photo' />
+      <h1 className='hero--header'>Online Experiences</h1>
+      <p className='hero--text'>Join unique interactive activities led by one-of-a-kind hosts—all without leaving home.</p>
+    </section>
+  )
+}
