6bf44aba3d031e1302ae0e6b08b3a51f2f574cba
Gordon <gordnaman@gmail.com>
2022-12-04 22:31:26 +0800

    fake-meme init

---
+++ study_react/fake-meme/public/components/Header.jsx
@@ -0,0 +1,7 @@
+export default function Header() {
+  return (
+    <header>
+      <h2>hello meme!</h2>
+    </header>
+  )
+}
---
+++ study_react/fake-meme/src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import Header from '../public/components/Header'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header />
+    </div>
+  )
+}
+
+export default App
