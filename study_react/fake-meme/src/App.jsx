f6d1748ad9e82eeecb2c986201d172229c8256e8
Gordon <gordnaman@gmail.com>
2022-12-09 00:26:56 +0800

    fake-meme done

--- study_react/fake-meme/src/App.jsx
+++ study_react/fake-meme/src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import Header from '../public/components/Header'
+import Meme from '../public/components/Meme'
 
 function App() {
 
   return (
     <div className="App">
       <Header />
+      <Meme />
     </div>
   )
 }
