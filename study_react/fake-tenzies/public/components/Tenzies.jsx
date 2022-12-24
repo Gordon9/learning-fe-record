6a7b622227988587abd02784524bda1b28a9cf37
Gordon <gordnaman@gmail.com>
2022-12-24 22:32:13 +0800

    just push

--- study_react/fake-tenzies/public/components/Tenzies.jsx
+++ study_react/fake-tenzies/public/components/Tenzies.jsx
@@ -1,21 +1,23 @@
 export default function Tenzies() {
   return (
-    <div>
-      <h3>Tenzies</h3>
-      <p>Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
-      <div className="dies--container">
-        <span>hello 1</span>
-        <span>hello 2</span>
-        <span>hello 3</span>
-        <span>hello 4</span>
-        <span>hello 5</span>
-        <span>hello 6</span>
-        <span>hello 7</span>
-        <span>hello 8</span>
-        <span>hello 9</span>
-        <span>hello 10</span>
+    <div className="container">
+      <div className="main">
+        <h3 className="game--title">Tenzies</h3>
+        <p className="game--describe">Roll until all dice are the same. Click each die to freeze it at its current value between rolls.</p>
+        <div className="dies--container">
+          <span>hello 1</span>
+          <span>hello 2</span>
+          <span>hello 3</span>
+          <span>hello 4</span>
+          <span>hello 5</span>
+          <span>hello 6</span>
+          <span>hello 7</span>
+          <span>hello 8</span>
+          <span>hello 9</span>
+          <span>hello 10</span>
+        </div>
+        <button>Roll</button>
       </div>
-      <button>Roll</button>
     </div>
   )
 }
