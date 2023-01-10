b404ea75cd20a446c30fc9c46a9adaa9c4fc229b
Gordon <gordnaman@gmail.com>
2023-01-10 16:35:06 +0800

    footer added

--- portfolio/gordontan/pages/index.js
+++ portfolio/gordontan/pages/index.js
@@ -11,8 +11,8 @@ export default function Home() {
         <meta name="description" content="GORDONTAN" />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
-      <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40">
-        <section className="min-h-screen">
+      <main className="bg-white px-10 dark:bg-gray-900 md:px-20 lg:px-40 min-h-screen">
+        <section>
           <nav className="py-10 mb-12 flex justify-between dark:text-white items-center">
             <h1 className="font-burtons text-xl">gordontan</h1>
             <ul className="flex items-center">
@@ -22,7 +22,7 @@ export default function Home() {
                   className="cursor-pointer text-2xl"
                 />
               </li>
-              <li className="px-4">
+              <li className="pl-8">
                 <Image
                   className="cursor-pointer rounded-full"
                   src={profilePic}
@@ -35,7 +35,26 @@ export default function Home() {
             </ul>
           </nav>
         </section>
+        <section className="py-10">
+          <div>
+            <h3 className="text-3xl py-1 dark:text-white">Portofolio</h3>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              Gordon伤心欲绝给诺兰寄刀片
+              <br />
+              Gordon不悔爱root顺手飞刀诺兰
+              <br />
+              Gordon凝望Netflix
+            </p>
+            <p className="text-md py-2 leading-8 text-gray-800 dark:text-gray-200">
+              sometime less is more
+            </p>
+          </div>
+        </section>
       </main>
+
+      <footer>
+        <p className="flex items-center">&copy;gordontan 2023</p>
+      </footer>
     </>
   );
 }
