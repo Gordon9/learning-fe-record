32464abd4c927fb078b915e1297e617652a43b7b
Gordon <gordnaman@gmail.com>
2023-02-06 14:13:21 +0800

    reg-demo done

---
+++ hm_tutorial/07_webapi/reg-demo/app.js
@@ -0,0 +1,18 @@
+const form = document.querySelector("#form");
+const input = document.querySelector("#number");
+const output = document.querySelector("#output");
+
+const re = /^(?:\d{3}|\(\d{3}\))([-/.])\d{3}\1\d{4}$/;
+
+function testInfo(phoneInput) {
+  const ok = re.exec(phoneInput.value);
+
+  output.textContent = ok
+    ? `Thanks, your phone number is ${ok[0]}`
+    : `${phoneInput.value} isn't a phone number with area code!`;
+}
+
+form.addEventListener("submit", (event) => {
+  event.preventDefault();
+  testInfo(input);
+});
