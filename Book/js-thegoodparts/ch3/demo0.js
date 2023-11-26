a55e1bff1659c99d08b0254148f67382d04f577d
Gordon <gordnaman@gmail.com>
2023-11-26 17:32:49 +0800

    ch3 done

---
+++ Book/js-thegoodparts/ch3/demo0.js
@@ -0,0 +1,23 @@
+const MYAPP = {};
+
+MYAPP.stooge = {
+  "first-name": "John",
+  "last-name": "Howard",
+};
+
+MYAPP.flight = {
+  airline: "Oceanic",
+  number: 815,
+  departure: {
+    IAIT: "SYD",
+    time: "2004-09-22 14:55",
+    city: "Sydney",
+  },
+  arrival: {
+    IAIT: "HELLO",
+    time: "2023-11-26 14:55",
+    city: "Guangzhou",
+  },
+};
+
+console.log(MYAPP);
